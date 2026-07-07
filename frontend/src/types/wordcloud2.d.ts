b937201dbfa0a723bc23700ee96d1c2e65cec699
jcrendon0909 <93552285+jcrendon0909@users.noreply.github.com>
2026-07-07 11:32:23 -0600

    fix: agregar typeRoots para declaración de wordcloud2

---
+++ frontend/src/types/wordcloud2.d.ts
@@ -0,0 +1,7 @@
+// frontend/src/types/wordcloud2.d.ts
+declare module 'wordcloud2' {
+  // La función que se exporta por defecto
+  function WordCloud(container: HTMLElement, options: any): void;
+  
+  export default WordCloud;
+}
